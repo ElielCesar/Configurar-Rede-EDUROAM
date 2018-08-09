e68abd39e47ac726db8d0597b0d64248c90f83a3
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2018-08-09 17:25:17 -0400

    Add files via upload

---
+++ configurar_eduroam.ps1
@@ -0,0 +1,40 @@
+﻿##[Ps1 To Exe]
+##
+##Kd3HDZOFADWE8uK1
+##Nc3NCtDXThU=
+##Kd3HFJGZHWLWoLaVvnQnhQ==
+##LM/RF4eFHHGZ7/K1
+##K8rLFtDXTiW5
+##OsHQCZGeTiiZ4NI=
+##OcrLFtDXTiW5
+##LM/BD5WYTiiZ4tI=
+##McvWDJ+OTiiZ4tI=
+##OMvOC56PFnzN8u+Vs1Q=
+##M9jHFoeYB2Hc8u+Vs1Q=
+##PdrWFpmIG2HcofKIo2QX
+##OMfRFJyLFzWE8uK1
+##KsfMAp/KUzWJ0g==
+##OsfOAYaPHGbQvbyVvnQX
+##LNzNAIWJGmPcoKHc7Do3uAuO
+##LNzNAIWJGnvYv7eVvnQX
+##M9zLA5mED3nfu77Q7TV64AuzAgg=
+##NcDWAYKED3nfu77Q7TV64AuzAgg=
+##OMvRB4KDHmHQvbyVvnQX
+##P8HPFJGEFzWE8tI=
+##KNzDAJWHD2fS8u+Vgw==
+##P8HSHYKDCX3N8u+Vgw==
+##LNzLEpGeC3fMu77Ro2k3hQ==
+##L97HB5mLAnfMu77Ro2k3hQ==
+##P8HPCZWEGmaZ7/K1
+##L8/UAdDXTlaDjofG5iZk2U7iS20nUuGeqr2zy5GA+u75qSrYRKYXRlpxny7DAU+oS/0TWLsQrNRx
+##Kc/BRM3KXxU=
+##
+##
+##fd6a9f26a06ea3bc99616d4851b372ba
+##Desenvolvido por Eliel Cesar, CLAB-Ariquemes
+
+##Habilita a execucao de scritps powershell
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+
+##Importa o perfil da rede eduroam
+netsh wlan add profile filename="$env:USERPROFILE\Desktop\eduroam\Wi-Fi-eduroam.xml" user=current
